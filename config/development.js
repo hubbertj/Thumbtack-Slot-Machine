b8a4f0c41d882d4b62090a1e1195c029467ce8ac
Jerum Hubbert <hubbertj@gmail.com>
2015-03-04 15:31:58 -0600

    03-04-2015-3-31
    Added files to start the server and worked the logic to having multi
    servers running.

---
+++ config/development.js
@@ -0,0 +1,18 @@
+//module is native node
+module.exports = {
+
+    sites: {
+        thumbtackSite: {
+            enableServer: true,
+            serverRoot: '/www',
+            port: 3000,
+            name: "Thumbtack Slot Machine"
+        },
+        homeSite: {
+            enableServer: true,
+            serverRoot: '/www2',
+            port: 4000,
+            name: "Thumbtack Slot Machine2s"
+        }
+    }
+};
---
+++ server-main.js
@@ -0,0 +1,41 @@
+//  Server main file
+
+'use strict';
+
+var path = require('path');
+var express = require('express');
+var logger = appLogger();
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var router = require('router');
+
+
+module.exports = {
+    serverSettings: null,
+    setSettings: function(svrSettings) {
+        this.serverSettings = svrSettings;
+    },
+
+    startServering: function() {
+        var app = new express();
+  
+        try {
+
+            app.set('port', this.serverSettings.port);
+
+
+            // static assets
+            app.use(express.static(path.join(__dirname, GLOBAL.APP_ROOT + this.serverSettings.serverRoot)));
+            app.use(express.static(GLOBAL.APP_ROOT + this.serverSettings.serverRoot));
+
+
+            app.listen(app.get('port'));
+
+            logger.info('Serving booted');
+            return true;
+        } catch (err) {
+            logger.error(err.message);
+            return false;
+        }
+    }
+}
